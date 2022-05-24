d30f4fca51e84853f2178d31492f161b8130f8dc
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-05-24 14:31:38 +0800

    Update class name

--- app/src/main/java/com/example/browsefoodapp/adapter/home/OverPopularMealAdapter.kt
+++ app/src/main/java/com/example/browsefoodapp/adapter/home/OverPopularMealAdapter.kt
@@ -7,20 +7,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.browsefoodapp.adapter.util.OverPopularMealDiffUtil
 import com.example.browsefoodapp.databinding.RvItemsTemplateBinding
-import com.example.browsefoodapp.model.theMealDb.MealByCategorySeaFood
+import com.example.browsefoodapp.model.theMealDb.MealSeaFood
 
-class OverPopularMealAdapter : RecyclerView.Adapter<OverPopularMealAdapter.HomeFragmentViewHolder>() {
+class OverPopularMealAdapter :
+    RecyclerView.Adapter<OverPopularMealAdapter.HomeFragmentViewHolder>() {
 
-    private var overPopularMealList = emptyList<MealByCategorySeaFood>()
+    lateinit var onItemClick: ((MealSeaFood) -> Unit)
+    private var overPopularMealList = emptyList<MealSeaFood>()
+
     /*
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder).
      */
-    inner class HomeFragmentViewHolder(val binding: RvItemsTemplateBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class HomeFragmentViewHolder(val binding: RvItemsTemplateBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
-    fun setOverPopularMeal(overPopularMealList:ArrayList<MealByCategorySeaFood>)
-    {
-        val overPopularMealListDiffUtil = OverPopularMealDiffUtil(overPopularMealList,this.overPopularMealList)
+    fun setOverPopularMeal(overPopularMealList: ArrayList<MealSeaFood>) {
+        val overPopularMealListDiffUtil =
+            OverPopularMealDiffUtil(overPopularMealList, this.overPopularMealList)
         val diffResults = DiffUtil.calculateDiff(overPopularMealListDiffUtil)
         this.overPopularMealList = overPopularMealList
         diffResults.dispatchUpdatesTo(this)
@@ -36,19 +40,29 @@ class OverPopularMealAdapter : RecyclerView.Adapter<OverPopularMealAdapter.HomeF
         val binding = RvItemsTemplateBinding.inflate(
             inflater,
             parent,
-            false)
+            false
+        )
 
         return HomeFragmentViewHolder(binding)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: HomeFragmentViewHolder, position: Int) {
-        Glide.with(holder.itemView)
-            .load(overPopularMealList[position].strMealThumb)
-            .into(holder.binding.ivRandomMeal)
+        updateItemImage(holder, position)
+
+        holder.itemView.setOnClickListener {
+            onItemClick.invoke(overPopularMealList[position])
+        }
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)-
     override fun getItemCount() = overPopularMealList.size
+
+    private fun updateItemImage(holder: HomeFragmentViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(overPopularMealList[position].strMealThumb)
+            .into(holder.binding.ivRandomMeal)
+    }
 
 }
